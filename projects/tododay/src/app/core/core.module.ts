c26b536da57cfb34af7725f263b592304d17ea17
Tim Oerlemans <tim@oerlemans.dev>
2025-03-30 16:55:41 +0200

    feat(angular): upgrade to Angular v18
    - Update Angular core packages from v17.3.12 to v18.2.13\n- Update Angular CLI from v17.3.13 to v18.2.16\n- Update TypeScript from v5.3.3 to v5.5.4\n- Add eslint-plugin-tsdoc for TSDoc documentation validation\n- Add @types/jest for TypeScript Jest definitions\n- Update HTTP modules to use provider functions

--- projects/tododay/src/app/core/core.module.ts
+++ projects/tododay/src/app/core/core.module.ts
@@ -1,18 +1,12 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 /**
  * Core module containing singleton services and application-wide providers.
  * This module should only be imported in the root AppModule.
  */
-@NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: []
-})
+@NgModule({ imports: [CommonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class CoreModule {
   private static guardConstructor = false;
 
